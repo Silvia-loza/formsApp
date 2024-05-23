cabf94c1affcc38ce97f0743f70141412f1ef8ec
Silvia Lozano <sylvilozano@gmail.com>
2024-05-23 14:59:28 +0200

    feat; Added validators for basic form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -14,16 +19,45 @@ export class BasicPageComponent {
   // });
 
   public myForm = this.fb.group({
-    name: ['', [], []],
-    price: [0, [], []],
-    inStorage: [0, [], []],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
   constructor(private fb: FormBuilder) {}
-  ngOnInit() {
-    console.log('El componente se ha inicializado');
+  ngOnInit() {}
+
+  isValidField(field: keyof typeof this.myForm.controls): boolean | null {
+    return (
+      this.myForm.controls[field].errors &&
+      this.myForm.controls[field] &&
+      this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: any): any {
+    const control = this.myForm.get(field);
+    if (!control) return null; // Control no encontrado
+    const errors = control.errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+
+      return null;
+    }
+
+    return control.errors || {};
   }
 
   onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log('My formulario valores ', this.myForm.value);
+    this.myForm.reset();
   }
 }

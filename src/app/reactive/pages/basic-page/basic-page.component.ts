c27c78b21a36976e77b9bfbe3d302d5203cdd32a
Silvia Lozano <sylvilozano@gmail.com>
2024-05-23 11:16:55 +0200

    feature: Added Reactive form to basic page

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
 export class BasicPageComponent {
-  constructor() {
-    console.log('BasicPageComponent loaded');
-  }
+  //Manera alternativa de settear un formulario Reactivo
+  // public myForm: FormGroup = new FormGroup({
+  // name: new FormControl('', [], []),
+  // price: new FormControl(0, [], []),
+  // inStorage: new FormControl(0, [], []),
+  // });
 
+  public myForm = this.fb.group({
+    name: ['', [], []],
+    price: [0, [], []],
+    inStorage: [0, [], []],
+  });
+  constructor(private fb: FormBuilder) {}
   ngOnInit() {
     console.log('El componente se ha inicializado');
+  }
+
+  onSave(): void {
+    console.log('My formulario valores ', this.myForm.value);
   }
 }

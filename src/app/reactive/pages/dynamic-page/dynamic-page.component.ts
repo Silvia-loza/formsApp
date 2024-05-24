3ef8f13c23944ddbd8d07d22a8330244b1e65d4a
Silvia Lozano <sylvilozano@gmail.com>
2024-05-24 10:53:55 +0200

    feat: added reactive form initial set up for dynamic form

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class DynamicPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      ['Metal Gear', 'Death Stranding', 'Gears of War', 'Horizon Zero Dawn'],
+      Validators.required
+    ),
+  });
+  constructor(private fb: FormBuilder) {}
 
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log('Mis datos de formulario enviados ', this.myForm.value);
+    this.myForm.reset();
+  }
 }

afe8bb78755995350e8366e77e2b2298544fbde5
Silvia Lozano <sylvilozano@gmail.com>
2024-05-27 12:21:04 +0200

    feature: added validations and delte function for dynamic form

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -14,6 +14,51 @@ export class DynamicPageComponent {
     ),
   });
   constructor(private fb: FormBuilder) {}
+
+  get favouriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+  isValidField(field: keyof typeof this.myForm.controls): boolean | null {
+    return (
+      this.myForm.controls[field].errors &&
+      this.myForm.controls[field] &&
+      this.myForm.controls[field].touched
+    );
+  }
+  getFieldError(field: any): any {
+    const control = this.myForm.get(field);
+    if (!control) return null; // Control no encontrado
+    const errors = control.errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+      return null;
+    }
+    return control.errors || {};
+  }
+
+  isValidFieldInArray(formArray: FormArray, i: number): boolean {
+    const formArrayInFUn = formArray;
+    const controlName = formArrayInFUn.at(i);
+    console.log('1. Control', controlName);
+    const controlErrors = this.myForm.controls['favoriteGames'].errors;
+    let isValid = !(
+      controlErrors && this.myForm.controls['favoriteGames'].touched
+    );
+
+    if (!isValid) {
+      isValid = false;
+    }
+    return isValid;
+  }
+
+  onDeleteFavourtieGame(i: number) {
+    this.favouriteGames.removeAt(i);
+  }
 
   onSubmit() {
     if (this.myForm.invalid) {

f3b0059457dd77a652137c756b14a16391d45d41
Silvia Lozano <sylvilozano@gmail.com>
2024-05-28 12:54:50 +0200

    feature: set up register form and added validators helper

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class RegisterPageComponent {
-  constructor() {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    username: [
+      '',
+      [Validators.required, Validators.minLength(3), cantBeStrider],
+    ],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(private fb: FormBuilder) {
     console.log('Register page');
+  }
+
+  isValidField(field: string) {
+    //obtener validacion desde servicio
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+
+      console.log('My form invalid');
+      return;
+    }
+
+    console.log('My form values', this.myForm.value);
   }
 }
